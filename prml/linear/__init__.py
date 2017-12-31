97f9cad2c0159f8c2c5481a9be4d090b535c72a5
Yusuke Yasuda <yusuke.007.yasud@gmail.com>
2017-12-31 20:36:24 +0900

    Regularized least squares

--- prml/linear/__init__.py
+++ prml/linear/__init__.py
@@ -1,7 +1,9 @@
 # https://github.com/ctgk/PRML/blob/master/prml/linear/__init__.py
 
 from prml.linear.linear_regressor import LinearRegressor
+from prml.linear.ridge_regressor import RidgeRegressor
 
 __all__ = [
     "LinearRegressor",
+    "RidgeRegressor",
 ]
